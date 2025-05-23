e28ebf29d42f9c3d9f77127db930abaecc96595e
Karina Arichavala <karina.arichavala@epn.edu.ec>
2025-05-22 22:22:20 -0500

    add: changes at pom.xml, add of dependencies, plugin and properties

---
+++ src/test/java/com/miempresa/mockito_tutorial/MockitoTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miempresa.mockito_tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockitoTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
